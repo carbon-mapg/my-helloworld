313d43627a637f4f6478e42c7ff46846bb1d9e4f
Marco Guerra <marcoguerra@carbonbybold.com>
2015-09-14 15:13:18 +0100

    First commit

---
+++ myhelloworld-lib/src/main/java/com/my/myhelloworldlib/MyHelloWorld.java
@@ -0,0 +1,10 @@
+package com.my.myhelloworldlib;
+
+/**
+ * MyHelloWorld by Carbon by BOLD
+ * Created in 14/09/15 by the following authors:
+ * <p/>
+ * Marco Guerra - {marcoguerra@carbonbybold.com}
+ */
+public class MyHelloWorld {
+}
