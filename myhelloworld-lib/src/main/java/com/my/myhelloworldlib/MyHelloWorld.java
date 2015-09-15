74b1a79bcdd9ac0c729fb4c7e966cba3134915db
Marco Guerra <marcoguerra@carbonbybold.com>
2015-09-15 11:39:16 +0100

    Updated MyHelloWorld

--- myhelloworld-lib/src/main/java/com/my/myhelloworldlib/MyHelloWorld.java
+++ myhelloworld-lib/src/main/java/com/my/myhelloworldlib/MyHelloWorld.java
@@ -7,4 +7,36 @@ package com.my.myhelloworldlib;
  * Marco Guerra - {marcoguerra@carbonbybold.com}
  */
 public class MyHelloWorld {
+
+    private int myInt;
+    private String myString;
+    private boolean myBoolean;
+
+    public MyHelloWorld() {
+
+    }
+
+    public int getMyInt() {
+        return myInt;
+    }
+
+    public void setMyInt(int myInt) {
+        this.myInt = myInt;
+    }
+
+    public String getMyString() {
+        return myString;
+    }
+
+    public void setMyString(String myString) {
+        this.myString = myString;
+    }
+
+    public boolean isMyBoolean() {
+        return myBoolean;
+    }
+
+    public void setMyBoolean(boolean myBoolean) {
+        this.myBoolean = myBoolean;
+    }
 }
